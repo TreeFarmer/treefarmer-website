f874f3fc8f67a5affb0b52678c92a3b420a5ac44
Fyrlex <fyrlex@gmail.com>
2021-09-14 21:23:36 -0400

    init - ty macedonga

---
+++ src/assets/Footer.js
@@ -0,0 +1,11 @@
+function App(props) {
+    return (<>
+        <footer className="w-full p-8 -mt-1">
+            <div className="max-w-7xl mx-auto px-8">
+                
+            </div>
+    </footer>
+    </>);
+}
+
+export default App;
