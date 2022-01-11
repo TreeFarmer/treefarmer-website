b44d287dc9b5182070043b2b254b6fbe8b35f7b7
Marco <me@marco.win>
2022-01-11 13:21:37 +0100

    Updates Added copyright and luyx.dev link on footer; Fixed header buttons.

--- src/assets/Footer.js
+++ src/assets/Footer.js
@@ -1,8 +1,10 @@
 function App(props) {
 	return (<>
-		<footer className="w-full p-8 -mt-1">
-			<div className="max-w-7xl mx-auto px-8">
-
+		<footer className="w-full p-8 -mt-1 main">
+			<div className="max-w-7xl mx-auto px-8 text-center font-semibold">
+				&copy; {new Date().getFullYear()} Tree Farmer - All rights reserved.
+				<br />
+				Developed with <span className="text-red-500">❤</span> by <a className="hover:underline" href="https://luyx.dev/" target="_blank" rel="noopener noreferrer">Luyx Development</a>.
 			</div>
 		</footer>
 	</>);
--- src/assets/Header.js
+++ src/assets/Header.js
@@ -1,3 +1,5 @@
+import { DocumentTextIcon, ShoppingCartIcon } from "@heroicons/react/solid";
+
 function App(props) {
 	return (<>
 		<header className="w-full p-8 -mb-1">
@@ -7,15 +9,17 @@ function App(props) {
 					<span className="ml-4 m-auto font-semibold text-sm lg:text-2xl">Tree Farmer</span>
 				</div>
 
-				<div className="ml-auto my-auto">
+				<div className="ml-auto my-auto flex">
 					<form action="/docs">
-						<button className="py-2 px-4 lg:px-8 font-semibold rounded-lg focus:ring flex">
-							Guide
+						<button className="py-2 px-2 lg:px-8 font-semibold rounded-lg focus:ring flex mr-4">
+							<span className="hidden lg:block">Guide</span>
+							<DocumentTextIcon className="w-6 h-6 lg:hidden" />
 						</button>
 					</form>
 					<form action="/shop">
-						<button className="py-2 px-4 lg:px-8 font-semibold rounded-lg focus:ring flex">
-							Shop
+						<button className="py-2 px-2 lg:px-8 font-semibold rounded-lg focus:ring flex">
+							<span className="hidden lg:block">Shop</span>
+							<ShoppingCartIcon className="w-6 h-6 lg:hidden" />
 						</button>
 					</form>
 				</div>
